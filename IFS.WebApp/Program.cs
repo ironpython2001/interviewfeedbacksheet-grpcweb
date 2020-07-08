5548c20ca3b05f9ac96ad0084106a72335ca2785
SunilP <spottumuttu@osius.com>
2020-07-08 15:42:25 +0530

    initial version

---
+++ IFS.WebApp/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components;
+using Grpc.Net.Client;
+
+namespace IFS.WebApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(services =>
+            {
+                // Create a gRPC-Web channel pointing to the backend server
+                var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+                var baseUri = services.GetRequiredService<NavigationManager>().BaseUri;
+                var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
+
+                // Now we can instantiate gRPC clients for this channel
+                return new Greeter.GreeterClient(channel);
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
