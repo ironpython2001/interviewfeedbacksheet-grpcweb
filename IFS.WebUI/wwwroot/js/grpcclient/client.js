5827a3275a308d7fda0adcecbeb49edecceda6d8
SunilP <spottumuttu@osius.com>
2021-08-17 18:29:34 +0530

    added

---
+++ IFS.WebUI/wwwroot/js/grpcclient/client.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="greet_pb.js" />
+/// <reference path="greet_grpc_web_pb.js" />
+const { HelloRequest, HelloReply } = require('./greet_pb.js');
+const { GreeterClient } = require('./greet_grpc_web_pb.js');
+
+var client = new GreeterClient('http://localhost:5001');
+
+var request = new HelloRequest();
+request.setName('World');
+
+client.sayHello(request, {}, (err, response) => {
+    console.log(response.getMessage());
+});
